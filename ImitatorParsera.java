2c9f2ee1573e0ff2eac70772b74af22e7be0a5e0
Polina <donotreplay@example.com>
2016-04-30 21:46:05 +0300

    Initial commit

---
+++ ImitatorParsera.java
@@ -0,0 +1,23 @@
+package imitatorparsera;
+
+public class ImitatorParsera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kolvo = 90;
+		System.out.println(parser(kolvo));
+	}
+	
+	public static String parser(int x){
+	
+		if(x<0){
+			return("Вас вызывают на границу!");
+		}
+		else{
+			return("в очереди перед Вами " + x + " машин." );
+		}
+		
+		 
+	} 
+
+}
